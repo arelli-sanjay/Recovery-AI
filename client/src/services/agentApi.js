732666af4a61da555d84f02f87c368e23eb08cd7
ARELLISANJAY <arellisanjay1105@gmail.com>
2026-09-04 23:43:45 +0530

    Initial commit

--- client/src/services/agentApi.js
+++ client/src/services/agentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://revenue-recovery-ai-server.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 // GET ALL AGENT CASES
 export const getAgentCases = async () => {
