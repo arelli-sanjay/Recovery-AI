18c5cf16345ab3bbb5b41e42f84908ec80e2f142
ARELLISANJAY <arellisanjay1105@gmail.com>
2026-09-04 23:37:14 +0530

    Update agentApi.js

--- client/src/services/agentApi.js
+++ client/src/services/agentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://revenue-recovery-ai-server.onrender.com/api";
 
 // GET ALL AGENT CASES
 export const getAgentCases = async () => {
