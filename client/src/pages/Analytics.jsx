b3683bc55ed51d4589a1baca3185f8ff1c54efc5
ARELLISANJAY <arellisanjay1105@gmail.com>
2026-09-05 07:01:21 +0530

    I

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -15,7 +15,7 @@ import {
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function Analytics() {
   const [analytics, setAnalytics] = useState(null);
